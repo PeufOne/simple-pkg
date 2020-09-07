83b0eaf162ff2fe9ac80b8f01aedc93dfab24601
Voisard <VOISARD_JO@swatchgroup.net>
2020-09-07 10:21:26 +0200

    Add public dir and serve static

--- app.js
+++ app.js
@@ -4,22 +4,17 @@ const filget = require('figlet')
 
 const app = express()
 
-app.get('/', (req, res, next) => {
-    res.send('Welcome')
-})
-
-app.get('/:txt', (req, res, next) => {
-    filget(req.params.txt, (err, data) => {
-        if (err) return res.send(`<pre>${data}</pre>`)
-        res.send(`<pre>${data}</pre>`)
-    })
-})
+app.use(express.static('public'))
 
 app.listen(5000, () => {
 
     filget('Simple-pkg', (err, data) => {
         if (err) return console.dir(err)
         console.log(data)
+        console.log()
+        console.log(`Bonjour, je suis le serveur de l'application MV-Manager.`)
+        console.log(`Ouvrez un navigateur (Edge ou Chrome) à l'addresse http://localhost:5000/`)
+        console.log()
     })
     open('http://localhost:5000/')
 
